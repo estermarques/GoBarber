fce1102a19570decc8a0a90b64590875ec4a8d0e
Ester Marques <estermarques@id.uff.br>
2020-10-05 15:45:23 -0300

    iniciando com o mongdb

--- src/modules/appointments/infra/http/routes/providers.routes.ts
+++ src/modules/appointments/infra/http/routes/providers.routes.ts
@@ -2,12 +2,27 @@ import { Router } from 'express';
 
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 import ProvidersController from '../controllers/ProvidersController';
+import ProviderMonthAvailavilityController from '../controllers/ProviderMonthAvailavilityController';
+import ProviderDayAvailavilityController from '../controllers/ProviderDayAvailavilityController';
 
 const providersRouter = Router();
+
 const providersController = new ProvidersController();
+const providerMonthAvailavilityController = new ProviderMonthAvailavilityController();
+const providerDayAvailavilityController = new ProviderDayAvailavilityController();
 
 providersRouter.use(ensureAuthenticated);
 
 providersRouter.get('/', providersController.index);
+
+providersRouter.get(
+  '/:provider_id/month-availability',
+  providerMonthAvailavilityController.index,
+);
+
+providersRouter.get(
+  '/:provider_id/day-availability',
+  providerDayAvailavilityController.index,
+);
 
 export default providersRouter;
